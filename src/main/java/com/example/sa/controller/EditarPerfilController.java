9a782fad7384fc57f03f1af960130f2b11c3d678
gbaoliveira <gbaoliveirasilva@gmail.com>
2023-11-17 10:08:23 -0300

    commit

--- src/main/java/com/example/sa/controller/EditarPerfilController.java
+++ src/main/java/com/example/sa/controller/EditarPerfilController.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.sa.controller;
 
-/**
- *
- * @author gabriel_ao_silva
- */
+import com.example.sa.model.Usuario; 
+import com.example.sa.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class EditarPerfilController {
-    
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Autowired
+    public EditarPerfilController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @PostMapping("/api/editarPerfil")
+    public ResponseEntity<?> editarPerfil(@RequestBody Map<String, String> body) {
+        String email = body.get("email");
+        String novoNome = body.get("nome");
+
+        Usuario usuario = usuarioRepository.findByEmail(email);
+        if (usuario != null) {
+            usuario.setNome(novoNome);
+            usuarioRepository.save(usuario);
+            return new ResponseEntity<>(usuario, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Usuário não encontrado", HttpStatus.NOT_FOUND);
+        }
+    }
 }
+    
